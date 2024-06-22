477b7e4b67959478c5fdcba7fff48c0f386094d1
Shingai-220424 <OCA2204240009@edu.oca.ac.jp>
2024-06-22 17:41:44 +0900

    しらね

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from test_model import Person
+from test_model import Human
 
 app = Flask(__name__)
 # Pythonを使用しての開発_Webアプリ2のpowerpointスライド26
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -52,9 +53,17 @@ def try_rest():
 @app.route('/person_search')
 def person_search():
     return render_template('./person_search.html')
-
 @app.route('/person_result')
 def person_result():
     search_size = request.args.get("search_size")
     persons = db.session.query(Person).filter(Person.size > search_size)
     return render_template('./person_result.html', persons=persons, search_size=search_size)
+@app.route('/human-search')
+def human_search():
+    return render_template('./human_search.html')
+@app.route('/human-result')
+def human_result():
+    search_height = request.args.get("search-height")
+    search_weight = request.args.get("search-weight")
+    humans = db.session.query(Human).filter(Human.height > search_height and Human.weight > search_weight)
+    return render_template('./human_result.html', humans = humans, search_height=search_height, search_weight=search_weight)
